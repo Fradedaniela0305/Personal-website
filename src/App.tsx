11a4aa0d2b4a02752cf20b9e99e59ddb03b944f3
Daniela Frade <fradenogdaniela@gmail.com>
2026-04-04 12:33:19 -0700

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./MainPage";
 import SoftwareDeveloper from "./SoftwareDeveloper";
 import Artist from "./Artist";
-import Drawing from "./Drawing"
-import Music from "./Music"
+
+
 
 export default function App() {
     return (
@@ -13,8 +13,8 @@ export default function App() {
                 <Route path="/developer" element={<SoftwareDeveloper />} />
                 <Route path="/artist" element={<Artist />} />
                 <Route path="/artist" element={<Artist />} />
-                <Route path="/artist/drawing" element={<Drawing />} />
-                <Route path="/artist/music" element={<Music />} />
+
+
             </Routes>
         </BrowserRouter>
     );
